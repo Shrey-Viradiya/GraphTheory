0e2a2755f5bbe2745d07aaeda6701a3059b88945
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2021-01-07 14:24:47 +0530

    Union and Intersection added

--- Prac1_AdjList.cpp
+++ Prac1_AdjList.cpp
@@ -4,10 +4,25 @@ using namespace std;
 
 int main(){
 
-    UndirectedGraph g(4);
-    g.addEdge(0,1);
-    g.addEdge(1,2);
-    g.displayGraph();
+    cout << "GraphA" << endl;
+    UndirectedGraph graphA(4);
+    graphA.addEdge(0,1);
+    graphA.addEdge(1,2);
+    graphA.displayGraph();
+
+    cout << "GraphB" << endl;
+    UndirectedGraph graphB(4);
+    graphB.addEdge(1,2);
+    graphB.addEdge(2,3);
+    graphB.displayGraph();
+
+    cout << "UnionGraph" << endl;
+    UndirectedGraph UnionGraph = UndirectedGraph::Union(graphA, graphB);
+    UnionGraph.displayGraph();
+
+    cout << "IntersectionGraph" << endl;
+    UndirectedGraph IntersectionGraph = UndirectedGraph::Intersection(graphA, graphB);
+    IntersectionGraph.displayGraph();
 
     return 0;
 }
--- UndirectedGraph.h
+++ UndirectedGraph.h
@@ -1,32 +1,36 @@
 #pragma once
-#include <vector>
+#include<vector>
+#include<set>
 #include<iterator>
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 class UndirectedGraph
-{ 
-    int numVertices;
-    vector <vector <int>> graph;
-    
-    
+{
 public:
+    int numVertices;
+    vector <set <int>> graph;
     UndirectedGraph(int V);
     void addEdge(int src, int dest);
     void displayGraph();
+    static UndirectedGraph Union(UndirectedGraph graphA, UndirectedGraph graphB);
+    static UndirectedGraph Intersection(UndirectedGraph graphA, UndirectedGraph graphB);
 };
 
 UndirectedGraph::UndirectedGraph(int V){
     numVertices = V;
     for (int i = 0; i < numVertices; i++)
     {
-        graph.push_back(vector<int> {i});
+        graph.push_back(set<int> {});
     }   
 }
 
 void UndirectedGraph::displayGraph(){
+    int node = 0;
     for (auto i = graph.begin(); i != graph.end(); i++)
     {
+        cout << (node++) << " -> ";
         for (auto j = (*i).begin(); j != (*i).end(); j++)
         {
             cout << *j << " -> ";
@@ -36,6 +40,30 @@ void UndirectedGraph::displayGraph(){
 }
 
 void UndirectedGraph::addEdge(int src, int dest){
-    graph[src].push_back(dest);
-    graph[dest].push_back(src);
+    graph[src].insert(dest);
+    graph[dest].insert(src);
+}
+
+UndirectedGraph UndirectedGraph::Union(UndirectedGraph graphA, UndirectedGraph graphB){
+    int V = graphA.numVertices;
+    UndirectedGraph union_graph(V);
+
+    for (int i = 0; i < V; i++)
+    {
+        set_union(graphA.graph[i].begin(), graphA.graph[i].end(), graphB.graph[i].begin(), graphB.graph[i].end(), inserter(union_graph.graph[i], union_graph.graph[i].begin()));
+    }
+
+    return union_graph;    
+}
+
+UndirectedGraph UndirectedGraph::Intersection(UndirectedGraph graphA, UndirectedGraph graphB){
+    int V = graphA.numVertices;
+    UndirectedGraph intersection_graph(V);
+
+    for (int i = 0; i < V; i++)
+    {
+        set_intersection(graphA.graph[i].begin(), graphA.graph[i].end(), graphB.graph[i].begin(), graphB.graph[i].end(), inserter(intersection_graph.graph[i], intersection_graph.graph[i].begin()));
+    }
+
+    return intersection_graph;    
 }
